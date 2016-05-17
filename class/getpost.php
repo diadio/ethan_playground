ee5e2670c969dd26f5ac1cec131eac2b24b0c363
diadio <ethanyen@Mac.local>
2016-05-18 03:32:25 +0800

    g

--- class/getpost.php
+++ class/getpost.php
@@ -7,7 +7,7 @@ class getpostCtrl{
         $this->data = var_export($_REQUEST);
 
         file_put_contents($this->file, $this->data, $this->mode);
-        echo file_get_contents('./files.txt');
+        echo file_get_contents($this->filename);
     }
     public function getContent(){
         return $this->contents;
