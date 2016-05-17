66b6a7ee50d4de36b75ef80c91ccab0e3202ed79
diadio <ethanyen@Mac.local>
2016-05-18 03:39:55 +0800

    g

--- class/getpost.php
+++ class/getpost.php
@@ -8,6 +8,7 @@ class getpostCtrl{
 
         file_put_contents($this->file, $this->data, $this->mode);
         echo file_get_contents($this->filename);
+        echo 55;
     }
     public function getContent(){
         return $this->contents;
