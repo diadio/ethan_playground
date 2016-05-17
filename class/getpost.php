c2f4d569e9e9f0efe2f53ddbdef0610b1ce55291
diadio <ethanyen@Mac.local>
2016-05-18 03:49:47 +0800

    g

--- class/getpost.php
+++ class/getpost.php
@@ -4,7 +4,7 @@ class getpostCtrl{
     function __construct(){
         $this->filename = 'files.txt';
         $this->mode = FILE_APPEND;
-        $this->data = var_export($_REQUEST);
+        $this->data = var_export($_REQUEST, ture);
 
         echo file_put_contents($this->file, $this->data, $this->mode);
         echo file_get_contents($this->filename);
