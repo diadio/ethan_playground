7ec9e19f9c5d05842fc76ce89438151afb6d15dc
ethan Yen <diadio_g@yahoo.com.tw>
2016-05-17 18:06:44 +0800

    5

--- class/getpost.php
+++ class/getpost.php
@@ -2,7 +2,11 @@
 
 class getpostCtrl{
     function __construct(){
-        var_dump($_REQUEST);
+        $this->filename = 'files.txt';
+        $this->mode = FILE_APPEND;
+        $this->data = var_export($_REQUEST);
+
+        file_put_contents($this->file, $this->data, $this->mode)
     }
     public function getContent(){
         return $this->contents;
