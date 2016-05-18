65f2ba58848582f48c02fe4db40e395598d38071
ethan Yen <diadio_g@yahoo.com.tw>
2016-05-18 11:38:15 +0800

    5

--- class/getpost.php
+++ class/getpost.php
@@ -4,11 +4,12 @@ class getpostCtrl{
     function __construct(){
         $this->filename = 'files.txt';
         $this->mode = FILE_APPEND;
-        $this->data = var_export($_REQUEST, ture);
+        $this->data = var_export($_REQUEST, ture).var_export(getallheaders(), ture);
 
     }
     public function saveContent(){
         file_put_contents($this->filename, $this->data, $this->mode);
+
         echo file_get_contents($this->filename);
         echo "<div>THE END -SAVED</div>";
         return true;
