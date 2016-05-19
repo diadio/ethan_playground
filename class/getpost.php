615f55f76302be69c2b14f8c0e821904861d3fef
ethan Yen <diadio_g@yahoo.com.tw>
2016-05-19 17:05:16 +0800

    5

--- class/getpost.php
+++ class/getpost.php
@@ -26,7 +26,10 @@ class getpostCtrl{
         $data_arr = array("form_id"=>$form, "msg"=>$msg, "time"=>time());
         $data = json_encode($data_arr).PHP_EOL;
         for($i=0; $i<10; $i++)
-            $this->sendDataLToLine($form, $msg);
+        {
+            sleep(30);
+            $this->sendDataLToLine($form, $msg.time());
+        }
         file_put_contents($this->filename, $data, $this->mode);
         echo file_get_contents($this->filename);
         echo "<div>THE END -SAVED</div>";
